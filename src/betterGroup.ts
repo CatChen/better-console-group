1a69a26890a45fc6ad151c62f42e7d460fd1b507
Cat Chen <catchen@catchen.me>
2025-04-16 18:06:52 -0700

    Fixed deno.json and for deno lint (#2)

--- src/betterGroup.ts
+++ src/betterGroup.ts
@@ -1,10 +1,10 @@
 export function betterGroup<T>(
   label: string,
   callbackFn: () => T,
-  thisArg?: any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  thisArg?: unknown,
 ): T {
   console.group(label);
-  const result: T = callbackFn.call(thisArg ?? global); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+  const result: T = callbackFn.call(thisArg ?? globalThis); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
   console.groupEnd();
   return result;
 }
